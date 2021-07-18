57b1db779e82ad87264eca7efc7dfe729b5bb885
Linhanrr <hank093011@gmail.com>
2021-07-18 15:58:16 +0800

    fix version

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.TextMessage
+    msg = event.message.text
     s = '你吃飯了嗎'
     line_bot_api.reply_message(
         event.reply_token,
